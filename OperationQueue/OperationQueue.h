297a0ef20a40694c452094ac69328795d0945a27
xulinfeng <xulinfeng@bilibili.com>
2018-05-24 18:09:33 +0800

    Fixed issues.

--- OperationQueue/OperationQueue.h
+++ OperationQueue/OperationQueue.h
@@ -6,11 +6,11 @@
 //  Copyright © 2018年 markejave. All rights reserved.
 //
 
-//! Project version number for MDOperationQueue.
-FOUNDATION_EXPORT double MDOperationQueueVersionNumber;
+//! Project version number for OperationQueue.
+FOUNDATION_EXPORT double OperationQueueVersionNumber;
 
-//! Project version string for MDOperationQueue.
-FOUNDATION_EXPORT const unsigned char MDOperationQueueVersionString[];
+//! Project version string for OperationQueue.
+FOUNDATION_EXPORT const unsigned char OperationQueueVersionString[];
 
-#import <MDOperationQueue/MDOperation.h>
-#import <MDOperationQueue/MDOperationQueue.h>
+#import <OperationQueue/MDOperation.h>
+#import <OperationQueue/MDOperationQueue.h>
